98bc35c98a3579a3f4437355b5f1a76c245bc00a
Marcin Pestka <pestka.m.j@gmail.com>
2023-02-14 01:22:35 +0100

    css

--- scripts/content.js
+++ scripts/content.js
@@ -9,19 +9,23 @@ function myFunction(){
       
     for (var i = 0; i < element.length; i++) {
         var button = document.createElement('button');
-        button.innerHTML = 'click me';
+        button.innerHTML = 'Copy';
+        button.style.color = "#d1d5db";
+        button.style.backgroundColor = "#2e2e2e";
+        button.style.borderRadius = "5px";
+        button.style.padding = "5px";
 
         with ({ n: i }) {
             button.onclick = function(event){
+                // TO DO - LISTS SOMETIMES RENDER AS <OL>
                 const AllP = this.parentElement.getElementsByTagName("p");
                 let text = "";
                 for (var j = 0; j < AllP.length; j++){
                     text += AllP[j].innerHTML;
                 }
+
                 navigator.clipboard.writeText(text).then(() => {
-                    //clipboard successfully set
                 }, () => {
-                    //clipboard write failed, use fallback
                 });
             };
           }
